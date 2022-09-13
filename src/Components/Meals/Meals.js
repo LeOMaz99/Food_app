0e395b0648704645c1b95ace90b301d4f210930c
LeOMaz99 <levan.maziashvili99@gmail>
2022-09-13 13:03:09 +0400

    adding Meals components

---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import AvailableMeals from "./AvailabelMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = (props) => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+export default Meals;
